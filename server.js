94a014a1bb6da76557a26f0e6dfcdb69deb18fea
Duc Nguyen <minhducng93@gmail.com>
2016-11-02 21:44:19 +0700

    Add DELETE method

--- server.js
+++ server.js
@@ -44,13 +44,27 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function (req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
 	if (!_.isBoolean(body.completed) || !_.isString(body.description)|| body.description.trim().length() === 0){
 		return res.status(400).send();
 	} 
+	body.description = body.description.trim();
+
 	body.id = todoNextId++;
 	todos.push(body);
 	res.json(body);
+});
+
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if (matchedTodo){
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	} else {
+		res.status(404).json({"Error": "No todo found with that ID"});
+	}
 });
 
 app.listen(PORT, function () {
