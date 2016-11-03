e62e9bee402c74d84ae35b96f30833e04cd90362
Duc Nguyen <minhducng93@gmail.com>
2016-11-03 20:32:38 +0700

    Add DELETE method to sequelize

--- server.js
+++ server.js
@@ -108,17 +108,32 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
-	if (matchedTodo) {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	} else {
-		res.status(404).json({
-			"Error": "No todo found with that ID"
-		});
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted){
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: "No todo with id"
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function(e){
+		res.status(500).send();
+	})
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+	// if (matchedTodo) {
+	// 	todos = _.without(todos, matchedTodo);
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).json({
+	// 		"Error": "No todo found with that ID"
+	// 	});
+	// }
 });
 
 app.put('/todos/:id', function(req, res) {
